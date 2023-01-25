6fd8d34e00e19ff7b6aab33dff534bfed0e05c38
alice west <alicewest.create@gmail.com>
2023-01-25 13:51:23 +0000

    comments tidied

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,13 +1,16 @@
-
-
+//    HTML VARIABLES
 
 const wrapperEl = document.querySelector(".wrapper")
 const highScoresContainer = document.querySelector("#highscores")
 const clearButton = document.querySelector("#clear")
 
 
+// SCRIPT
+
+// On load, get the value of entries from the local storage
 const entriesArray = JSON.parse(localStorage.getItem("entries"))
 
+// For each entry, create a list item, with the value of the entries inside the item and append that to the highscore container
 entriesArray.forEach(entry => {
     const listItem = document.createElement("li")
     listItem.innerHTML = "Name: " + entry.name + " | Score : " + entry.score
@@ -15,16 +18,13 @@ entriesArray.forEach(entry => {
     
 });
 
+// This clears the local store and hides the score on the page. 
 const clearStorage = function(){
     localStorage.removeItem("entries")
     highScoresContainer.classList.add("hide")
-
-
 }
 
+// EVENT LISTENER 
 
 clearButton.addEventListener("click", clearStorage)
-
-
-
 
